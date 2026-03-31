dbde970f1e194fbce930ab2ee5805f11c581d80b
pasyaa <mhmdpsyh@gmail.com>
2026-03-31 19:34:30 +0700

    rerata = (nilB+nilM)/2;

---
+++ Looping/LogikaOr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika=";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Ingghris=";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+    
+}
