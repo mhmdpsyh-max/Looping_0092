038785ee47a182ef7c46879a3853081b4eb60359
pasyaa <mhmdpsyh@gmail.com>
2026-03-31 19:02:50 +0700

    status = "lulus";

--- Looping/LogikaNot.cpp
+++ Looping/LogikaNot.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 int main() {
     float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika =";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris =";
+    cin>>nilB;
+
+    rerata = (nilB=nilM)/2;
+
+    if (!(rerata < 60))
+        status = "lulus";
+
+    cout<<
 }
