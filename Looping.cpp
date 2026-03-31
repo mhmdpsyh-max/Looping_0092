d89b211e04e058d08b756c6fc3c5e9cff09da493
pasyaa <mhmdpsyh@gmail.com>
2026-03-31 18:52:38 +0700

    float nilB,nilM,rerata;

--- Looping.cpp
+++ Looping.cpp
@@ -2,4 +2,33 @@
 #include <ctime>
 using namespace std;
 
-int main (){}
+int main() {
+    int x;
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0; i<=4; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+    srand(time(0));
+
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while (x<=5){
+        cout<<"Bilangan Acak = "<<x <<endl;
+        x = rand ()%10;
+    }
+    
+    cout<<"Bilangan Acak while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    cout<<"PERULANGAN DO...WHILE"<<endl;
+    x = 1 + rand ()%10;
+    do{
+        cout<<"Bilangan acak ="<<x <<endl;
+        x= rand ()%10; 
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir="<<x <<endl;
+    cout<<endl;
+}
